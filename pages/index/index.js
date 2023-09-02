f87849cbc011c54e33fb3b5cc9e31fefe0382ab9
WumaCoder <15804854160@163.com>
2023-09-02 15:20:33 +0800

    feat: 修改名称

--- pages/index/index.js
+++ pages/index/index.js
@@ -60,7 +60,7 @@ Page({
     onShareAppMessage: function () {
         let info = wx.getStorageSync('userInfo');
         return {
-            title: '海风小店',
+            title: '源华品购',
             desc: '开源微信小程序商城',
             path: '/pages/index/index?id=' + info.id
         }
