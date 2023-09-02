6a7c81604103bc512e87eba4b91fd1a2274a43b7
WumaCoder <15804854160@163.com>
2023-09-02 15:15:46 +0800

    chore: 提炼环境配置

--- config/api.js
+++ config/api.js
@@ -1,6 +1,7 @@
-const ApiRoot = 'http://localhost:8360';
+const env = require('../env');
+// const ApiRoot = 'http://localhost:8360';
 // const ApiRoot = 'http://192.168.0.113:8361';
-// const ApiRoot = 'https://www.qile.club:8688';
+const ApiRoot = env.ApiRoot;
 const ApiRootUrl = ApiRoot + '/api/'
 
 module.exports = {
